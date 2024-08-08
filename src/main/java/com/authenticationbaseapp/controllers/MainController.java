954fc04b4f4db815cda399a89c7555a7461c15d1
VladimirFilim <boterfly6@gmail.com>
2024-08-09 03:17:01 +0400

    bugfix with access to endpoint /main for unathorized user. Added loggingCount in /main page

--- src/main/java/com/authenticationbaseapp/controllers/MainController.java
+++ src/main/java/com/authenticationbaseapp/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.authenticationbaseapp.controllers;
 
 import com.authenticationbaseapp.services.LoggedUserManagementService;
+import com.authenticationbaseapp.services.LoginCountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +12,12 @@ import java.util.logging.Logger;
 @Controller
 public class MainController {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
     Logger logger = Logger.getLogger(this.getClass().getName());
 
-    public MainController(LoggedUserManagementService loggedUserManagementService) {
+    public MainController(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
@@ -22,17 +25,19 @@ public class MainController {
             @RequestParam(required = false) String Logout,
             Model model
     ) {
+
         if(Logout != null) {
-            loggedUserManagementService.setUsername("null");
-            return "redirect:/";
+            loggedUserManagementService.setUsername(null);
         }
 
         String username = loggedUserManagementService.getUsername();
+        int loginCount = loginCountService.getCount();
         if (username == null) {
             logger.info("Access denied!");
             return "redirect:/";
         }
         model.addAttribute("username", username);
+        model.addAttribute("loginCount", loginCount);
         return "main.html";
     }
 }
--- src/main/java/com/authenticationbaseapp/processors/LoginProcessor.java
+++ src/main/java/com/authenticationbaseapp/processors/LoginProcessor.java
@@ -1,6 +1,7 @@
 package com.authenticationbaseapp.processors;
 
 import com.authenticationbaseapp.services.LoggedUserManagementService;
+import com.authenticationbaseapp.services.LoginCountService;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
@@ -17,21 +18,27 @@ public class LoginProcessor {
     Logger logger = Logger.getLogger(LoginProcessor.class.getName());
 
     private LoggedUserManagementService loggedUserManagementService;
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    private LoginCountService loginCountService;
+
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService,
+                          LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     private String username;
     private String password;
 
     public boolean login() {
+        loginCountService.increment();
+
         String password = this.password;
         String username = this.username;
         logger.info(username + " is trying to login with password: " + password);
 
         boolean loggingResult = false;
 
-        if("natalie".equals(username) && "1234".equals(password)) {
+        if ("natalie".equals(username) && "1234".equals(password)) {
             loggedUserManagementService.setUsername(username);
             loggingResult = true;
             logger.info("Logged in successfully, username: " + this.username);
